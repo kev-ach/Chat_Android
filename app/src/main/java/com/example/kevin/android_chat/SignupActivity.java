c0f41beb07316a1a3862306522967fc1f7f41cf7
namtruong2711 <36995131+namtruong2711@users.noreply.github.com>
2018-04-24 21:27:10 +0200

    progress dialog login et inscription

--- app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
@@ -1,10 +1,12 @@
 package com.example.kevin.android_chat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,7 +28,7 @@ public class SignupActivity extends AppCompatActivity {
     private EditText mTelephone;
     private Button mSignupButton;
     private Toolbar mToolbar;
-
+    private ProgressDialog mRegProgressDialog;
 
     private FirebaseAuth mAuth;
 
@@ -36,6 +38,8 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mRegProgressDialog = new ProgressDialog(this);
 
         mLoginInput = (EditText) findViewById(R.id.signup_main_login);
         mPasswordInput = (EditText) findViewById(R.id.signup_main_mdp);
@@ -66,7 +70,22 @@ public class SignupActivity extends AppCompatActivity {
                     Toast.makeText(SignupActivity.this, "Veuillez insérer le même mot de passe.", Toast.LENGTH_LONG).show();
                 }
                 else  {
-                    register_user(nom,prenom,mail,mdp,telephone);
+                    if(!TextUtils.isEmpty(nom) ||
+                            !TextUtils.isEmpty(prenom) ||
+                            !TextUtils.isEmpty(mail) ||
+                            !TextUtils.isEmpty(telephone) ||
+                            !TextUtils.isEmpty(mdp) ||
+                            !TextUtils.isEmpty(mdp_confirm)){
+
+                        mRegProgressDialog.setTitle("Inscription en cours");
+                        mRegProgressDialog.setMessage("Veuillez patientez");
+                        mRegProgressDialog.setCanceledOnTouchOutside(false);
+                        mRegProgressDialog.show();
+                        register_user(nom,prenom,mail,mdp,telephone);
+                    }else {
+                        Toast.makeText(SignupActivity.this,"Veuillez remplir les champs",Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
@@ -80,10 +99,14 @@ public class SignupActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
+
+                            mRegProgressDialog.dismiss();
                             Intent mainIntent = new Intent(SignupActivity.this, ChatActivity.class);
                             startActivity(mainIntent);
                             finish();
                         } else {
+
+                            mRegProgressDialog.hide();
                             Toast.makeText(SignupActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                         }
 
