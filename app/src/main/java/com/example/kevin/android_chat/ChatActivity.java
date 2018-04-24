b990584a988ed85da99b55a18c985a8ee9650e82
namtruong2711 <36995131+namtruong2711@users.noreply.github.com>
2018-04-24 15:04:54 +0200

    Merge branch 'master' of C:\Users\Nam\AndroidStudioProjects\Chat_Android with conflicts.

--- app/src/main/java/com/example/kevin/android_chat/ChatActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/ChatActivity.java
@@ -3,6 +3,9 @@ package com.example.kevin.android_chat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +17,7 @@ public class ChatActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private Button modifierProfil;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,10 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolBar = (Toolbar) findViewById(R.id.chat_page_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Chat Android");
 
         modifierProfil = (Button) findViewById((R.id.activity_main_valider_btn));
 
@@ -41,9 +49,36 @@ public class ChatActivity extends AppCompatActivity {
 
         if(currentUser == null){
 
-            Intent startIntent = new Intent(ChatActivity.this, MainActivity.class);
-            startActivity(startIntent);
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+
+        Intent startIntent = new Intent(ChatActivity.this, MainActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.chat_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.chat_logout_btn){
+
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +25,8 @@ public class SignupActivity extends AppCompatActivity {
     private EditText mEmail;
     private EditText mTelephone;
     private Button mSignupButton;
+    private Toolbar mToolbar;
+
 
     private FirebaseAuth mAuth;
 
@@ -42,6 +45,11 @@ public class SignupActivity extends AppCompatActivity {
         mPrenom = (EditText) findViewById(R.id.signup_main_prenom);
         mTelephone = (EditText) findViewById(R.id.signup_main_telephone);
         mSignupButton = (Button) findViewById(R.id.signup_main_submit_btn);
+
+        mToolbar =(Toolbar) findViewById(R.id.register_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Inscription");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
