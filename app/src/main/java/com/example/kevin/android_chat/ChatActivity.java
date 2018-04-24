660fc9320c7b592c1f5684f64cb9691be7d23ca0
namtruong2711 <36995131+namtruong2711@users.noreply.github.com>
2018-04-24 11:32:08 +0200

    inscription

--- app/src/main/java/com/example/kevin/android_chat/ChatActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/ChatActivity.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private Button modifierProfil;
 
@@ -14,6 +19,8 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
 
         modifierProfil = (Button) findViewById((R.id.activity_main_valider_btn));
 
@@ -24,5 +31,19 @@ public class ChatActivity extends AppCompatActivity {
                 startActivity(profilActivityIntent);
             }
         }));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+
+            Intent startIntent = new Intent(ChatActivity.this, MainActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.kevin.android_chat;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,20 +9,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends AppCompatActivity {
 
     private EditText mLoginInput;
     private EditText mPasswordInput;
     private EditText mPasswordConfirmInput;
+    private EditText mNom;
+    private EditText mPrenom;
+    private EditText mEmail;
+    private EditText mTelephone;
     private Button mSignupButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
         mLoginInput = (EditText) findViewById(R.id.signup_main_login);
         mPasswordInput = (EditText) findViewById(R.id.signup_main_mdp);
         mPasswordConfirmInput = (EditText) findViewById(R.id.signup_main_mdpconfirm);
+        mEmail = (EditText) findViewById(R.id.signup_main_email);
+        mNom = (EditText) findViewById(R.id.signup_main_nom);
+        mPrenom = (EditText) findViewById(R.id.signup_main_prenom);
+        mTelephone = (EditText) findViewById(R.id.signup_main_telephone);
         mSignupButton = (Button) findViewById(R.id.signup_main_submit_btn);
 
         mSignupButton.setOnClickListener(new View.OnClickListener() {
@@ -30,16 +49,37 @@ public class SignupActivity extends AppCompatActivity {
 
                 String mdp = mPasswordInput.getText().toString();
                 String mdp_confirm = mPasswordConfirmInput.getText().toString();
+                String nom = mNom.getText().toString();
+                String prenom = mPrenom.getText().toString();
+                String mail = mEmail.getText().toString();
+                String telephone = mTelephone.getText().toString();
 
                 if (!mdp.equals(mdp_confirm)) {
                     Toast.makeText(SignupActivity.this, "Veuillez insérer le même mot de passe.", Toast.LENGTH_LONG).show();
                 }
                 else  {
-
-                    Intent chatActivityIntent = new Intent(SignupActivity.this, ChatActivity.class);
-                    startActivity(chatActivityIntent);
+                    register_user(nom,prenom,mail,mdp,telephone);
                 }
             }
         });
+    }
+
+    private void register_user(String nom, String prenom, String mail, String mdp, String telephone) {
+
+        mAuth.createUserWithEmailAndPassword(mail, mdp)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent mainIntent = new Intent(SignupActivity.this, ChatActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                        } else {
+                            Toast.makeText(SignupActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 }
