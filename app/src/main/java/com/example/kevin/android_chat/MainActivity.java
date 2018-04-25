6aa1531e75d5d5f0d4275edaf7cf935818a50a03
erwan.daly@gmail.com <Rocas419*3>
2018-04-25 12:15:33 +0200

    Fonction de login Firebase

--- app/src/main/java/com/example/kevin/android_chat/MainActivity.java
+++ app/src/main/java/com/example/kevin/android_chat/MainActivity.java
@@ -1,60 +1,103 @@
 package com.example.kevin.android_chat;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mLoginInput;
-    private EditText mPasswordInput;
+    private EditText mLoginInput,mPasswordInput;
     private Button mValiderButton;
     private Button mSignupButton;
+    private FirebaseAuth auth;
+
+    private Button btnSignup, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, ChatActivity.class));
+            finish();
+        }
+
+        // set the view now
         setContentView(R.layout.activity_main);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         mLoginInput = (EditText) findViewById(R.id.activity_main_login);
         mPasswordInput = (EditText) findViewById(R.id.activity_main_mdp);
-        mValiderButton = (Button) findViewById((R.id.activity_main_valider_btn));
-        mSignupButton = (Button) findViewById((R.id.activity_main_signup));
+        btnLogin = (Button) findViewById(R.id.activity_main_valider_btn);
+        btnSignup = (Button) findViewById(R.id.activity_main_signup);
 
-        mValiderButton.setOnClickListener(new View.OnClickListener() {
+
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                String mdp = mPasswordInput.getText().toString();
-                String login = mLoginInput.getText().toString();
-
-                if (login.equals("") && mdp.equals("")) {
-                    Toast.makeText(MainActivity.this, "Identifiant et/ou Mot de passe obligatoire", Toast.LENGTH_SHORT).show();
-                } else if (login.equals("") && mdp.equals("test")) {
-                    // Si mdp_local(le mot de passe entrer) = au mot de passe enregistrer dans SharedPreference alors redirige vers AddProjectActivity
-                        /*for (int i = 0; i < 2; i++) {
-                            Toast.makeText(MainActivity.this, "Mot de passe : " + mdp, Toast.LENGTH_SHORT).show();
-                        }*/
-                    Intent chatActivityIntent = new Intent(MainActivity.this, ChatActivity.class);
-                    startActivity(chatActivityIntent);
-                } else {
-                    for (int i = 0; i < 2; i++) {
-                        Toast.makeText(MainActivity.this, "Mot de passe incorrect", Toast.LENGTH_SHORT).show();
-                    }
-                }
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignupActivity.class));
             }
         });
 
-        mSignupButton.setOnClickListener((new View.OnClickListener() {
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent signupActivityIntent = new Intent(MainActivity.this, SignupActivity.class);
-                startActivity(signupActivityIntent);
+            public void onClick(View v) {
+                String email = mLoginInput.getText().toString();
+                final String password = mPasswordInput.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        mPasswordInput.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(MainActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
-        }));
+        });
     }
 }
